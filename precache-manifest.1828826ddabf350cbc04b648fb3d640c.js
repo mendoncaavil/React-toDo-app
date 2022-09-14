1a3bc3dabb0a3a1f75282ddf2115a2a6382c01ae
Avil Mendonca <mendoncaavil@gmail.com>
2022-09-14 12:53:30 +0530

    Updates

---
+++ precache-manifest.1828826ddabf350cbc04b648fb3d640c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b0c16eb37bb026d2e59",
+    "url": "/static/css/main.5194faf2.chunk.css"
+  },
+  {
+    "revision": "5b0c16eb37bb026d2e59",
+    "url": "/static/js/main.5b0c16eb.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "cb1335819c6f651c03ba",
+    "url": "/static/js/2.cb133581.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "30647db4de0abfbb4bf1d9527da8addb",
+    "url": "/index.html"
+  }
+];
